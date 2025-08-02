65db84f8b8503b20709e3edc72b4128a8b82345f
Varsha-gurram <varshini_gurram@srmap.edu.in>
2025-08-02 15:30:28 +0530

    solved database errors

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
+require('dotenv').config();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Connected to MongoDB in ${process.env.DEV_MODE} mode at host ${mongoose.connection.host}`.bgCyan.white);
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err}`.bgRed.white);
+    process.exit(1);
+  }
+};
 
-const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log(`connected to MongoDb ${mongoose.connection.host}`.bgCyan.white)
-    }catch(err){
-        console.log(`MongoDb database error: ${err}`.bgRed.white);
-    }
-}
-
-module.exports =connectDB;
+module.exports = connectDB;
